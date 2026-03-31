a7c358737103b59eeb31b44e54f18c813536d053
Tyler Phillips <Tyler@BusyBeesSeniorCare.com>
2026-03-31 15:24:11 -0400

    Simplify ESLint config to unblock Vercel build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
 
-const eslintConfig = defineConfig(
-  nextVitals,
-  nextTs,
+export default defineConfig([
   globalIgnores([
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
-  ])
-);
-
-export default eslintConfig;
+  ]),
+]);
